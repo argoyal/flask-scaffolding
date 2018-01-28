d647879244fdcf01457bc545353a4f8e185b7488
Arpit Goyal <arpitgoyal.thunderbird@gmail.com>
2018-01-29 00:10:36 +0530

    adding ipdb to requirements file

--- src/app.py
+++ src/app.py
@@ -8,5 +8,6 @@ SETTINGS_FILE = os.environ.get("SETTINGS_FILE", "settings.local_settings")
 
 application.config.from_object(SETTINGS_FILE)
 
+# Adding all the url rules in the api application
 for url, method, _ in all_urls:
     application.add_url_rule(url, view_func=method)
---
+++ src/scripts/__init__.py
@@ -0,0 +1,5 @@
+"""
+Scripts module for running the scripts as flask
+management commands. A sample example is shown
+in this file.
+"""
