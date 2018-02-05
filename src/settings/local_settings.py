7b422cf4688ecfd05b8625200067ba8ab08b6700
Arpit Goyal <arpitgoyal.thunderbird@gmail.com>
2018-02-05 18:25:39 +0530

    incorporated local settings in the flask scaffolding application

---
+++ src/settings/local_settings.py
@@ -0,0 +1,32 @@
+import os
+
+
+DEBUG = True
+LOG_ROOT = os.environ.get("LOG_ROOT")
+LOG_FILENAME = "flask-scaffolding.log"
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'formatters': {
+        'default': {
+            'format': '%(asctime)s %(levelname)s %(name)s %(message)s'
+        }
+    },
+    'handlers': {
+        'default': {
+            'level': 'DEBUG',
+            'class': 'logging.FileHandler',
+            'formatter': 'default',
+            'encoding': 'utf-8',
+            'filename': os.path.join(LOG_ROOT, LOG_FILENAME)
+        }
+    },
+    'loggers': {
+        'default': {
+            'handlers': ['default'],
+            'level': 'DEBUG',
+            'propogate': True,
+        }
+    }
+}
