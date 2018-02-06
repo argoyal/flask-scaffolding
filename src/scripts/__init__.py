184b71fb29834d3ebb1dde31a02b2932adf32632
Arpit Goyal <arpitgoyalhtmedia@users.noreply.github.com>
2018-02-06 13:07:04 +0530

    Update __init__.py
    Added explanation to the cli commands structure.

--- src/scripts/__init__.py
+++ src/scripts/__init__.py
@@ -2,6 +2,34 @@
 Scripts module for running the scripts as flask
 management commands. A sample example is shown
 in this file.
+
+Write your scripts in this folder and import as shown
+below for the test scripts. Support for command line parameters
+is needed. Add the imported new command in the ALL_CLI_COMMANDS
+parameter.
+
+For example:
+
+If you create a management cli command name createuser in the file
+user.py in scripts folder, the command can be run using flask createuser
+
+scripts/user.py
+--------------------------------
+import click
+
+@click.command
+def createuser():
+    # Create user code
+    print("Creating the user")
+    
+    
+scripts/__init__.py
+---------------------------------
+from .user import createuser
+
+ALL_CLI_COMMANDS = {
+    "createuser": createuser
+}
 """
 
 from .tests import test_cli_command
